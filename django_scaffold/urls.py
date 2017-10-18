530e7047c24ea9ab2cb952494e7df3615a5e661e
carloscba <carloscba1@gmail.com>
2017-10-18 17:20:48 -0300

    - Inicio

---
+++ django_scaffold/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from django.contrib import admin
+
+from rest_framework.routers import DefaultRouter
+
+from apps.profiles.views import *
+
+
+router = DefaultRouter()
+
+router.register('profile', ProfileViewSet)
+router.register('login', LoginViewSet, base_name='login')
+
+urlpatterns = [
+    url(r'', include(router.urls))
+]
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls))
+]
